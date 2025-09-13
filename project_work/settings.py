a37dcead3ddedb754502db595e621859ce289db5
vijaykumar7010 <vijayakumar04545@gmail.com>
2025-09-13 19:33:10 +0530

    cors policy added

--- project_work/settings.py
+++ project_work/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lzbj^-0&a0mfy41bjwhz25386fu&)4o&ejtmzg6cav6(uydn6h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apiview'
+    'apiview',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,3 +136,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CORS_ALLOWED_ORIGINS = [
+#         "http://localhost:3000",
+#         'https://test-backent-server.onrender.com'
+#     ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
