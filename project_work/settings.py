4539786e167eda77690e264bb9fd306c1b3ecd08
vijaykumar7010 <vijayakumar04545@gmail.com>
2025-09-13 19:28:43 +0530

    db config changed

--- project_work/settings.py
+++ project_work/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'project_work.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vj_dev_database',
+        'USER': 'vj_dev_database',
+        'PASSWORD': 'DkSyH92VghTBoGjQnHwUokUmKeEvcUE2',
+        'HOST': 'dpg-d32nd3fdiees738uctj0-a.singapore-postgres.render.com',  # Or the IP address/hostname of your PostgreSQL server
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
